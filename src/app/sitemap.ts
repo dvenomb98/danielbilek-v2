28760941de0e98e7889b73e8fdc42cdc032b8332
Daniel Bílek <daniel.bilek@regiojet.com>
2023-10-30 14:33:02 +0100

    Added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next'
+import { allPosts } from 'contentlayer/generated'
+import siteMetadata from '@/data/siteMetadata'
+import { navigationLinks } from '@/lib/consts/urls'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl
+  const blogRoutes = allPosts.map((post) => ({
+    url: `${siteUrl}/${post.url}`,
+    lastModified: post.date,
+  }))
+
+  const routes = navigationLinks.map((route) => ({
+    url: `${siteUrl}/${route.href}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...blogRoutes]
+}
